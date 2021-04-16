e93d0021f5e6b1c0746b824da847399035c679da
nishankayastha <58814673+nishankayastha@users.noreply.github.com>
2021-04-16 22:03:33 +0545

    Adding bootstrap through cdn

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <footer className="py-1">
+                <p className="text-center text-white mt-1">
+                    Killua store ©2021 All rights Reserved
+                </p>
+            </footer>
+        </Fragment>
+    )
+}
+
+export default Footer
+ 
